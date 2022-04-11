71b08c9e7ceb780de0805e48c7bab872987f58ce
ok-josue <87337858+ok-josue@users.noreply.github.com>
2022-04-10 21:42:49 -0700

    split forms into a search and an add form

--- public/javascript/add-teamMember.js
+++ public/javascript/add-teamMember.js
@@ -1,16 +1,3 @@
-// form.addEventListener('submit',function()) {
-//     const name  = document.getElementById('add-name').value
-//     const email = document.getElementById('add-email').value
-//     const role = document.getElementById('add-role').value
-
-//     console.log(name + ' ' + email + ' ' + role);
-// };
-
-
-
-const btn = document.getElementById('btn');
-btn.addEventListener('click',clickHandler);
-
-function clickHandler(event) {
-    console.log("Button Clicked");
-}
+document.getElementById("btn").addEventListener("click", function () {
+    alert("Hello World!");
+});

f7b3be4d757c9249a0c404b16b88475828207321
ok-josue <87337858+ok-josue@users.noreply.github.com>
2022-04-11 15:26:52 -0700

    trying to get add functionality working:

--- public/javascript/add-teamMember.js
+++ public/javascript/add-teamMember.js
@@ -1,3 +1,59 @@
-document.getElementById("btn").addEventListener("click", function () {
-    alert("Hello World!");
+// form.addEventListener('submit',function()) {
+//     const name  = document.getElementById('add-name').value
+//     const email = document.getElementById('add-email').value
+//     const role = document.getElementById('add-role').value
+
+//     console.log(name + ' ' + email + ' ' + role);
+// };
+
+
+
+$(document).ready(function () {
+    $("form").parsley();
 });
+
+$(function () {
+    $('#add-team-member')
+        .parsley().on("field:validated", function () {
+            var ok = $(".parsley-error").length === 0;
+            $(".bs-callout-info").toggleClass("hidden", !ok);
+            $(".bs-callout-warning").toggleClass("hidden", ok);
+        })
+        .on("form:submit", function () {
+            return false; // Don't submit form for this demo
+        });
+});
+
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click',clickHandler);
+
+function clickHandler(event) {
+    console.log("Button Clicked");
+}
+
+
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('add-team-member').value.trim();
+    const response = await fetch (`/api/user-routes`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name
+        }),
+        headers: { 
+            'Content-Type': 'application/json' 
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.getElementById('add-btn').addEventListener('click', newFormHandler)
+
+<script src="/javascript/add-teamMember.js"></script>
